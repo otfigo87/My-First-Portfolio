f4f2dd626ed1d2af3d53702caac84ca9f58c9e74
otmane aatik <aaotmane@gmail.com>
2021-02-10 22:37:56 -0500

    first commit

---
+++ script.js
@@ -0,0 +1,20 @@
+(function ($) {
+    var toggle = document.getElementById("menu-toggle");
+    var menu = document.getElementById("menu");
+    var close = document.getElementById("menu-close");
+
+    toggle.addEventListener("click", function (e) {
+        if (menu.classList.contains("open")) {
+            menu.classList.remove("open");
+            menu.style.backgroundColor ="#232323";
+        } else {
+            menu.classList.add("open");
+            
+        }
+    });
+
+    close.addEventListener("click", function (e) {
+        menu.classList.remove("open");
+    });
+
+})(jQuery);
